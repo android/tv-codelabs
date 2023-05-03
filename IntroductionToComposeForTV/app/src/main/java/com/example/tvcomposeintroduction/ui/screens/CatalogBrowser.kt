86cdaaece128325f3ce9a778de3a7365b5a9bd4b
Chiko Shimizu <chikoski@google.com>
2023-05-03 11:55:20 +0900

    Add the solution code
    Change-Id: I9f57eadd4b502c3ccd1f45f7a14275922823215c

--- IntroductionToComposeForTV/app/src/main/java/com/example/tvcomposeintroduction/ui/screens/CatalogBrowser.kt
+++ IntroductionToComposeForTV/app/src/main/java/com/example/tvcomposeintroduction/ui/screens/CatalogBrowser.kt
@@ -16,17 +16,100 @@
 
 package com.example.tvcomposeintroduction.ui.screens
 
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.tv.foundation.lazy.list.TvLazyColumn
+import androidx.tv.foundation.lazy.list.TvLazyRow
+import androidx.tv.foundation.lazy.list.items
+import androidx.tv.material3.Carousel
 import androidx.tv.material3.ExperimentalTvMaterial3Api
+import androidx.tv.material3.MaterialTheme
+import androidx.tv.material3.Text
+import coil.compose.AsyncImage
+import com.example.tvcomposeintroduction.R
 import com.example.tvcomposeintroduction.data.Movie
+import com.example.tvcomposeintroduction.ui.components.MovieCard
 
-@OptIn(ExperimentalTvMaterial3Api::class)
+@OptIn(ExperimentalTvMaterial3Api::class, ExperimentalAnimationApi::class)
 @Composable
 fun CatalogBrowser(
     modifier: Modifier = Modifier,
     catalogBrowserViewModel: CatalogBrowserViewModel = viewModel(),
     onMovieSelected: (Movie) -> Unit = {}
 ) {
+    val categoryList by catalogBrowserViewModel.categoryList.collectAsState()
+    TvLazyColumn(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(32.dp),
+        contentPadding =
+        PaddingValues(horizontal = 58.dp, vertical = 36.dp)
+    ) {
+        item {
+            val featuredMovieList by catalogBrowserViewModel.featuredMovieList.collectAsState()
+            Carousel(
+                itemCount = featuredMovieList.size,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(376.dp),
+            ) { indexOfCarouselItem ->
+                val featuredMovie = featuredMovieList[indexOfCarouselItem]
+                CarouselItem(
+                    background = {
+                        AsyncImage(
+                            model = featuredMovie.backgroundImageUrl,
+                            contentDescription = null,
+                            placeholder = painterResource(
+                                id = R.drawable.placeholder
+                            ),
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier.fillMaxSize(),
+                        )
+                    },
+                    modifier = Modifier.clickable {
+                        onMovieSelected(featuredMovie)
+                    }
+                ) {
+                    Text(
+                        text = featuredMovie.title,
+                        style =
+                        MaterialTheme.typography.headlineLarge,
+                        modifier = Modifier
+                            .padding(36.dp)
+                            .background(MaterialTheme.colorScheme.background)
+                    )
+                }
+            }
+        }
+        items(categoryList) { category ->
+            Text(text = category.name)
+            TvLazyRow(
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
+                modifier = Modifier.height(200.dp)
+            ) {
+                items(category.movieList) { movie ->
+                    MovieCard(
+                        movie,
+                        onClick = {
+                            onMovieSelected(it)
+                        }
+                    )
+                }
+            }
+        }
+    }
 }
--- IntroductionToComposeForTV/app/src/main/java/com/example/tvcomposeintroduction/ui/screens/Details.kt
+++ IntroductionToComposeForTV/app/src/main/java/com/example/tvcomposeintroduction/ui/screens/Details.kt
@@ -16,12 +16,48 @@
 
 package com.example.tvcomposeintroduction.ui.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
 import androidx.tv.material3.ExperimentalTvMaterial3Api
+import androidx.tv.material3.MaterialTheme
+import androidx.tv.material3.Text
+import coil.compose.AsyncImage
 import com.example.tvcomposeintroduction.data.Movie
 
 @OptIn(ExperimentalTvMaterial3Api::class)
 @Composable
 fun Details(movie: Movie, modifier: Modifier = Modifier) {
+    Box(modifier = modifier.fillMaxSize()) {
+        AsyncImage(
+            model = movie.cardImageUrl,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+        Column(
+            modifier = Modifier
+                .padding(horizontal = 58.dp, vertical = 36.dp)
+                .background(MaterialTheme.colorScheme.background),
+        ) {
+            Text(
+                text = movie.title,
+                style = MaterialTheme.typography.headlineLarge,
+            )
+            Text(
+                text = movie.studio,
+                style = MaterialTheme.typography.headlineMedium,
+            )
+            Text(
+                text = movie.title,
+                style = MaterialTheme.typography.headlineMedium,
+            )
+        }
+    }
 }
