f335a0e209d222aa9b7f706c066c904a0589dd26
N. Shimizu <chikoski@google.com>
2023-06-08 11:02:29 +0900

    Rebase solution code (#5)
    * Add README, CONTRIBUTING, LICESE, and .gitignore
    
    Change-Id: I108965ed7d5926200752e2d12700ef3448bb4bcd
    
    * Corrected URL to codelab
    
    * Upgrade AGP version (#2)
    
    Change-Id: I86e5162dc06c9b89c87b9c1d61bf60eedf9022b9
    
    * Dependency update.
    
    * Replace CarouselItem with Card due to the removal of the component.
    
    ---------
    
    Co-authored-by: Paul Lammertsma <pflammertsma@gmail.com>

--- IntroductionToComposeForTV/app/src/main/java/com/example/tvcomposeintroduction/MainActivity.kt
+++ IntroductionToComposeForTV/app/src/main/java/com/example/tvcomposeintroduction/MainActivity.kt
@@ -19,31 +19,12 @@ package com.example.tvcomposeintroduction
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.semantics.isContainer
-import androidx.compose.ui.semantics.semantics
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
 import androidx.tv.material3.ExperimentalTvMaterial3Api
-import androidx.tv.material3.LocalAbsoluteTonalElevation
-import androidx.tv.material3.LocalContentColor
-import androidx.tv.material3.MaterialTheme
-import androidx.tv.material3.contentColorFor
-import androidx.tv.material3.surfaceColorAtElevation
+import androidx.tv.material3.Surface
 import com.example.tvcomposeintroduction.ui.theme.TvComposeIntroductionTheme
 
 class MainActivity : ComponentActivity() {
@@ -54,80 +35,13 @@ class MainActivity : ComponentActivity() {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             TvComposeIntroductionTheme {
-                Container(
+                Surface(
+                    shape = RectangleShape,
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
                 ) {
                     App()
                 }
             }
         }
-    }
-}
-
-/**
- * Container works as a container of a component.
- */
-@OptIn(ExperimentalTvMaterial3Api::class)
-@Composable
-private fun Container(
-    modifier: Modifier = Modifier,
-    shape: Shape = RectangleShape,
-    color: Color = MaterialTheme.colorScheme.surface,
-    contentColor: Color = contentColorFor(color),
-    tonalElevation: Dp = 0.dp,
-    shadowElevation: Dp = 0.dp,
-    border: BorderStroke? = null,
-    content: @Composable () -> Unit = {}
-) {
-    val absoluteElevation = LocalAbsoluteTonalElevation.current + tonalElevation
-
-    CompositionLocalProvider(
-        LocalContentColor provides contentColor,
-        LocalAbsoluteTonalElevation provides absoluteElevation,
-    ) {
-        Box(
-            modifier = modifier
-                .surface(
-                    shape = shape,
-                    backgroundColor = surfaceColorAtElevation(
-                        color = color,
-                        elevation = absoluteElevation
-                    ),
-                    border = border,
-                    shadowElevation = shadowElevation
-                )
-                .semantics(mergeDescendants = false) {
-                    isContainer = true
-                }
-                .pointerInput(Unit) {},
-            propagateMinConstraints = true
-        ) {
-            content()
-        }
-    }
-}
-
-/**
- * A modifier to add show, outline with border, change the background color,and clip with given shape
- */
-private fun Modifier.surface(
-    shape: Shape,
-    backgroundColor: Color,
-    border: BorderStroke?,
-    shadowElevation: Dp
-) = this
-    .shadow(shadowElevation, shape, clip = false)
-    .then(if (border != null) Modifier.border(border, shape) else Modifier)
-    .background(color = backgroundColor, shape = shape)
-    .clip(shape)
-
-@OptIn(ExperimentalTvMaterial3Api::class)
-@Composable
-private fun surfaceColorAtElevation(color: Color, elevation: Dp): Color {
-    return if (color == MaterialTheme.colorScheme.surface) {
-        MaterialTheme.colorScheme.surfaceColorAtElevation(elevation)
-    } else {
-        color
     }
 }
---
+++ README.md
@@ -0,0 +1,36 @@
+# TV Codelabs
+
+This repository contains a set of Android Studio projects that are refered from codelabs for Android TV apps: 
+
+- [Introduction to Compose for TV](https://developer.android.com/codelabs/compose-for-tv-introduction)
+
+## Getting started
+
+1. Clone this repository.
+2. Open the specific codelab that you are interested in.
+3. Open the project associated with the codelab within Android Studio.
+
+## Additional resources
+
+- [Android TV introduction](https://www.android.com/tv/)
+- [Android TV Developer Documentation](https://developer.android.com/tv)
+- [Android TV Apps in Google Play Store](https://play.google.com/store/apps/collection/promotion_3000e26_androidtv_apps_all)
+
+
+## License
+
+```
+Copyright 2023 The Android Open Source Project
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+``` 
